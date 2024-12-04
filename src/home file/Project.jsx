900a4b1e1f16f4f8e122e52e8a3b5a7bc14a5143
minhazapon <minhajulabedinapon@gmail.com>
2024-12-03 20:12:35 -0800

    added file

--- src/home file/Project.jsx
+++ src/home file/Project.jsx
@@ -6,64 +6,98 @@ const Project = () => {
 
 
     return (
+
+        <div>
+
+
         <div className=" mb-20 mt-20 ml-16 mr-16 ">
-
-
         <div>
         <div>
         <p className=" text-6xl deep  text-center ">Latest Projects</p>
         <p className=" text-center  text-xl deep mt-5 text-gray-400 ">Getting online is easy. Succeeding online is a different story. You’ll needM<br></br> more than just a beautiful website to stand out these days.</p>
         </div>
         </div>
-
-
-
         <div>
-            <div className=" flex-col md:flex-row lg:flex-row mt-10 flex justify-between items-center gap-5">
-
-
-            <figure class="relative max-w-sm transition-all duration-300 cursor-pointer">
-              <a href="#">
-                <img className="rounded-lg" src="https://webdesign-finder.com/deepdigital-ai-v2/wp-content/uploads/elementor/thumbs/img_86-qu23e8rnnlnywqf5fnpjgmnu1nk9giknloyw4n0nko.jpg" alt="" />
-              </a>
-              <figcaption class="absolute px-4 text-lg text-white bottom-6">
-                  <p className=" deep bg-gradient-to-r from-black p-3 rounded-xl ">Robots are programmable machines designed to perform tasks autonomously or semi-autonomously.They combine electronic, and software systems.</p>
-              </figcaption>
-            </figure>
-
-
-
-            <figure class="relative max-w-sm transition-all duration-300 cursor-pointer">
-              <a href="#">
-                <img className="rounded-lg" src="https://webdesign-finder.com/deepdigital-ai-v2/wp-content/uploads/elementor/thumbs/img_82-qu23e8rnnlnywqf5fnpjgmnu1nk9giknloyw4n0nko.jpg" alt="" />
-              </a>
-              <figcaption class="absolute px-4 text-lg text-white bottom-6">
-                  <p className=" deep bg-gradient-to-r from-black p-3 rounded-xl ">A bike, short for "bicycle," is a human-powered or motor-powered vehicle with two wheels, widely used for transportation.</p>
-              </figcaption>
-            </figure>
-
-
-
-            <figure class="relative max-w-sm transition-all duration-300 cursor-pointer">
-              <a href="#">
-                <img className="rounded-lg" src="https://webdesign-finder.com/deepdigital-ai-v2/wp-content/uploads/elementor/thumbs/img_83-qu23e8rnnlnywqf5fnpjgmnu1nk9giknloyw4n0nko.jpg" alt="" />
-              </a>
-              <figcaption class="absolute px-4 text-lg text-white bottom-6">
-                  <p className=" deep bg-gradient-to-r from-black p-3 rounded-xl ">Knowledge is the collection of information, understanding, and skills acquired through experience, education, or discovery. </p>
-              </figcaption>
-            </figure>
-
-
-
-
-
-
-
-
-
-            </div>
+        <div className=" flex-col md:flex-row lg:flex-row mt-10 flex justify-between items-center gap-5">
+        <figure class="relative max-w-sm transition-all duration-300 cursor-pointer">
+        <a href="#">
+        <img className="rounded-lg" src="https://webdesign-finder.com/deepdigital-ai-v2/wp-content/uploads/elementor/thumbs/img_86-qu23e8rnnlnywqf5fnpjgmnu1nk9giknloyw4n0nko.jpg" alt="" />
+        </a>
+        <figcaption class="absolute px-4 text-lg text-white bottom-6">
+        <p className=" deep bg-gradient-to-r from-black p-3 rounded-xl ">Robots are programmable machines designed to perform tasks autonomously or semi-autonomously.They combine electronic, and software systems.</p>
+        </figcaption>
+        </figure>
+        <figure class="relative max-w-sm transition-all duration-300 cursor-pointer">
+        <a href="#">
+        <img className="rounded-lg" src="https://webdesign-finder.com/deepdigital-ai-v2/wp-content/uploads/elementor/thumbs/img_82-qu23e8rnnlnywqf5fnpjgmnu1nk9giknloyw4n0nko.jpg" alt="" />
+        </a>
+        <figcaption class="absolute px-4 text-lg text-white bottom-6">
+        <p className=" deep bg-gradient-to-r from-black p-3 rounded-xl ">A bike, short for "bicycle," is a human-powered or motor-powered vehicle with two wheels, widely used for transportation.</p>
+        </figcaption>
+        </figure>
+        <figure class="relative max-w-sm transition-all duration-300 cursor-pointer">
+        <a href="#">
+        <img className="rounded-lg" src="https://webdesign-finder.com/deepdigital-ai-v2/wp-content/uploads/elementor/thumbs/img_83-qu23e8rnnlnywqf5fnpjgmnu1nk9giknloyw4n0nko.jpg" alt="" />
+        </a>
+        <figcaption class="absolute px-4 text-lg text-white bottom-6">
+        <p className=" deep bg-gradient-to-r from-black p-3 rounded-xl ">Knowledge is the collection of information, understanding, and skills acquired through experience, education, or discovery. </p>
+        </figcaption>
+        </figure>
         </div>
-            
+        </div>
+        </div>
+
+
+
+
+        <div className=" -mt-32  bg-no-repeat bg-cover  p-20  "
+        style={{
+          backgroundImage: "url(https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFyayUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D)",
+        }}
+        
+        >
+           <div className=" ml-14 mr-14 flex-col md:flex-row lg:flex-row flex justify-between gap-16 items-center ">
+
+             <div>
+               <img className=" h-[400px] w-[350px]  " src="https://webdesign-finder.com/deepdigital-ai-v2/wp-content/uploads/2023/06/img_75.png" alt="" />
+             </div>
+
+  
+             <div className=" flex items-center ">
+             <div className=" grid  md:grid-cols-2 gap-12 text-white ">
+             <div>
+             <img className=" h-[40px] w-[35px] " src="https://webdesign-finder.com/deepdigital-ai-v2/wp-content/uploads/2023/06/img_76.png" alt="" />
+             <p className=" text-2xl mt-5 deep  ">All In One</p>
+             <p className=" mt-5  deep ">You can use the exact same movr<br></br> services, now complimentary</p>
+             </div>
+             <div>
+             <img className=" h-[40px] w-[35px] " src="https://webdesign-finder.com/deepdigital-ai-v2/wp-content/uploads/2023/06/img_77.png" alt="" />
+             <p className=" text-2xl mt-5 deep  ">Move Safe & Sound</p>
+             <p className=" mt-5  deep ">99% of our customers moved claim-free in<br></br> 2016</p>
+             </div>
+             <div>
+             <img className=" h-[40px] w-[35px] " src="https://cdn-icons-png.flaticon.com/128/8049/8049428.png" alt="" />
+             <p className=" text-2xl mt-5 deep  ">Save Money</p>
+             <p className=" mt-5  deep ">78% of our customers said they chose Us<br></br> because of price</p>
+             </div>
+             <div>
+             <img className=" h-[40px] w-[35px] " src="https://webdesign-finder.com/deepdigital-ai-v2/wp-content/uploads/2023/06/img_79.png" alt="" />
+             <p className=" text-2xl mt-5 deep  ">Experience Great Service</p>
+             <p className=" mt-5  deep ">We’ve earned a 4.5 star rating an A+ BBB<br></br> rating</p>
+             </div>
+             </div> 
+             </div> 
+
+           </div>
+        </div>
+
+
+
+
+
+
+
+
         </div>
     );
 };
